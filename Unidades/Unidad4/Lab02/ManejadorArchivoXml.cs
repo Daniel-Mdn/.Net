bc06e9feba7eb28c8056717ac1b31c2bc72c4c01
Daniel-Mdn <danielmedina012@gmail.com>
2022-06-09 20:26:15 -0300

    Lab02 realizado

---
+++ Unidades/Unidad4/Lab02/ManejadorArchivoXml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace Lab02
+{
+    class ManejadorArchivoXml: ManejadorArchivo
+    {
+        protected DataSet ds;
+        public override DataTable getTabla()
+        {
+            this.ds = new DataSet();
+            this.ds.ReadXml("agenda.xml");
+            return ds.Tables["contactos"];
+        }
+        public override void aplicaCambios()
+        {
+            this.ds.WriteXml("agenda.xml");
+            this.ds.WriteXml("agendaconschema.xml", XmlWriteMode.WriteSchema);
+
+        }
+    }
+}
