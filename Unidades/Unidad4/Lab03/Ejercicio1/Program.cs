e25c4d4173d139fb6316121e26b239faead7e4c8
Daniel-Mdn <danielmedina012@gmail.com>
2022-06-14 17:19:38 -0300

    Lab 03 realizado

---
+++ Unidades/Unidad4/Lab03/Ejercicio1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dtAlumnos = new DataTable("Alumnos");
+            DataColumn colNombre = new DataColumn("Nombre", typeof(string));
+            DataColumn colApellido = new DataColumn("Apellido", typeof(string));
+            dtAlumnos.Columns.Add(colApellido);
+            dtAlumnos.Columns.Add(colNombre);
+            Console.WriteLine("Listado de Alumnos");
+            DataRow rwAlumno = dtAlumnos.NewRow();
+            rwAlumno["Apellido"] = "Perez";
+            rwAlumno["Nombre"] = "Juan";
+
+            dtAlumnos.Rows.Add(rwAlumno);
+
+            DataRow rwAlumno2 = dtAlumnos.NewRow();
+            rwAlumno2["Apellido"] = "Perez";
+            rwAlumno2["Nombre"] = "Marcelo";
+            dtAlumnos.Rows.Add(rwAlumno2);
+
+            foreach (DataRow row in dtAlumnos.Rows)
+            {
+                Console.WriteLine(row["Apellido"].ToString() + ", " + row["Nombre"].ToString());
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
